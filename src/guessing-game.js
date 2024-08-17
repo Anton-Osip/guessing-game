f758eb6eb8ba95e6bfe8f9f1e45fced43d932bd4
Anton-Osip <osipchika@gmail.com>
2024-08-17 14:54:10 +0300

    feat: task is completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.number = null
+        this.min = null
+        this.max = null
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        this.number = Math.round(this.min + (this.max - this.min) / 2)
+        return this.number
     }
 
     lower() {
-
+        this.setRange(this.min, this.number)
     }
 
     greater() {
-
+        this.setRange(this.number, this.max)
     }
 }
 
